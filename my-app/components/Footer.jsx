d0b55d6c10a75c36b3a13c0114762b2da89b9239
Avinash <avinashkumarofficial95@gmail.com>
2025-06-13 11:43:19 +0530

    updated version of footer

--- my-app/components/Footer.jsx
+++ my-app/components/Footer.jsx
@@ -1,42 +1,53 @@
 import React from 'react'
-
+import Link from 'next/link'
 function Footer() {
   return (
-    <div>
+   <footer className='bg-gradient-to-t from-[#dce8f3] to-white text-gray-700 px-4 py-10'>
+    <div className='max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-10'>
+      <div className='col-span-1 flex items-start'>
         <img src="https://supreme-group.vercel.app/static/media/logo.68f5b8493efb88f7cd65756bf67a1f5b.svg" 
-        alt="logo" className='ml-12' />
-        <div className='pt-5 flex flex-row justify-around'>
-        <ul>
-            <h2 className='font-semibold mb-4'>APPLICATIONS</h2>
-            <li>Apparel</li>
-            <li>Automotive</li>
-            <li>Filtration</li>
-            <li>Customised Nonwoven</li>
-        </ul>
+        alt="Supreme Group Logo" 
+        className='w-28' />
+      </div>
+      <div className='md:col-span-4 grid grid-cols-2 md:grid-cols-4 gap-6 text-sm'>
+        <div>
+          <h4 className='font-bold mb-3 text-black'>APPLICATIONS</h4>
+          <ul className='space-y-2'>
+            <li><Link href='#' className='hover:text-black'>Apparel</Link></li>
+            <li><Link href='#' className='hover:text-black'>Automotive</Link></li>
+            <li><Link href='#' className='hover:text-black'>Filtration</Link></li>
+            <li><Link href='#' className='hover:text-black'>Customised Nonwoven</Link></li>
+          </ul>
+        </div>
+        <div>
+          <h4 className='font-bold mb-3 text-black'>COMPANY</h4>
           <ul>
-            <h2 className='font-semibold mb-4'>APPLICATIONS</h2>
-            <li>Apparel</li>
-            <li>Automotive</li>
-            <li>Filtration</li>
-            <li>Customised Nonwoven</li>
-        </ul>
-           <ul>
-            <h2 className='font-semibold mb-4'>APPLICATIONS</h2>
-            <li>Apparel</li>
-            <li>Automotive</li>
-            <li>Filtration</li>
-            <li>Customised Nonwoven</li>
-        </ul> 
-           <ul>
-            <h2 className='font-semibold mb-4'>APPLICATIONS</h2>
-            <li>Apparel</li>
-            <li>Automotive</li>
-            <li>Filtration</li>
-            <li>Customised Nonwoven</li>
-        </ul>  
-    </div>
-       
-    </div>
+            <li><Link href='#' className='hover:text-black'>Who We Are</Link></li>
+            <li><Link href='#' className='hover:text-black'>Global Competency</Link></li>
+            <li><Link href='#' className='hover:text-black'>Innovation</Link></li>
+            <li><Link href='#' className='hover:text-black'>ESG Impact</Link></li>
+          </ul>
+        </div>  
+        <div>
+          <h4 className='font-bold text-black mb-3'>MORE</h4>
+          <ul>
+            <li><Link href='#' className='hover:text-black'>Contact Us</Link></li>
+            <li><Link href='#' className='hover:text-black'>Careers</Link></li>
+          </ul>
+        </div>
+        <div>
+          <h4 className='font-bold text-black mb-3'>FOLLOW US</h4>
+          <ul>
+            <li><Link href='#' className='hover:text-black'>Linkedin</Link></li>
+          </ul>
+        </div>
+      </div>
+    </div> 
+    <div className='mt-10 pt-5 flex justify-around text-center  text-gray-600'>
+      <p>©2024. All Rights Reserved.</p>
+      <p>Supreme House, 110, 16th Road, Chembur, Mumbai – 400071.</p>
+    </div> 
+   </footer>
   )
 }
 
--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className='pt-5 pl-5'>
         <img src="https://supreme-group.vercel.app/static/media/logo.68f5b8493efb88f7cd65756bf67a1f5b.svg" 
-        alt="supreme-logo" />
+        alt="Supreme Group Logo" />
     </div>
   )
 }
