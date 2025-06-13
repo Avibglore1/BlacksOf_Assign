985d8d2180bff2d46fdcbc2eda7e94763d43d188
Avinash <avinashkumarofficial95@gmail.com>
2025-06-13 10:52:42 +0530

    prepared a skeleton of header and footer

---
+++ my-app/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <img src="https://supreme-group.vercel.app/static/media/logo.68f5b8493efb88f7cd65756bf67a1f5b.svg" 
+        alt="logo" className='ml-12' />
+        <div className='pt-5 flex flex-row justify-around'>
+        <ul>
+            <h2 className='font-semibold mb-4'>APPLICATIONS</h2>
+            <li>Apparel</li>
+            <li>Automotive</li>
+            <li>Filtration</li>
+            <li>Customised Nonwoven</li>
+        </ul>
+          <ul>
+            <h2 className='font-semibold mb-4'>APPLICATIONS</h2>
+            <li>Apparel</li>
+            <li>Automotive</li>
+            <li>Filtration</li>
+            <li>Customised Nonwoven</li>
+        </ul>
+           <ul>
+            <h2 className='font-semibold mb-4'>APPLICATIONS</h2>
+            <li>Apparel</li>
+            <li>Automotive</li>
+            <li>Filtration</li>
+            <li>Customised Nonwoven</li>
+        </ul> 
+           <ul>
+            <h2 className='font-semibold mb-4'>APPLICATIONS</h2>
+            <li>Apparel</li>
+            <li>Automotive</li>
+            <li>Filtration</li>
+            <li>Customised Nonwoven</li>
+        </ul>  
+    </div>
+       
+    </div>
+  )
+}
+
+export default Footer
---
+++ my-app/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='pt-5 pl-5'>
+        <img src="https://supreme-group.vercel.app/static/media/logo.68f5b8493efb88f7cd65756bf67a1f5b.svg" 
+        alt="supreme-logo" />
+    </div>
+  )
+}
+
+export default Header
